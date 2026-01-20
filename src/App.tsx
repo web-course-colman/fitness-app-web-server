6ed409805c1400cc7fc77d41e11504810d075903
guyYablonka <guyyablonka@gmail.com>
2026-01-20 23:29:48 +0200

    add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "./pages/NotFound";
 
 
 const queryClient = new QueryClient();
@@ -14,7 +15,7 @@ const App = () => (
             <Sonner />
             <BrowserRouter>
                 <Routes>
-
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
         </TooltipProvider>
